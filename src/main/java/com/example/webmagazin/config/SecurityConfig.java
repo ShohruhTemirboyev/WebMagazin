3c1029d6ff53437a5a0210ea439f8bfc50d86fe9
ShohruhTemirboyev <84275582+ShohruhTemirboyev@users.noreply.github.com>
2021-08-18 21:44:13 +0500

    s

--- src/main/java/com/example/webmagazin/config/SecurityConfig.java
+++ src/main/java/com/example/webmagazin/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/productType/**","/user/getUsers").hasRole("ADMIN")
+                .antMatchers("/user/getUsers").hasRole("ADMIN")
                 .antMatchers("/user/**","/product/**")
                 .permitAll()
                 .antMatchers("/**")
